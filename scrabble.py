fb9b6ac66dac54ab09c9a58b0dc88cee4d0b6c7f
vaskovo1 <antot777@gmail.com>
2022-08-27 23:56:05 +0300

    added description

--- scrabble.py
+++ scrabble.py
@@ -1,3 +1,43 @@
+"""
+Реализуйте функцию scrabble(), которая принимает два аргумента в следующем
+порядке:
+
+    symbols — набор символов
+    word — слово
+
+Функция должна возвращать True, если из набора символов symbols можно составить
+слово word, или False в противном случае.
+
+При проверке учитывается количество символов, которые нужны для
+составления слова, и не учитывается их регистр.
+
+
+Sample Input 1:
+
+print(scrabble('bbbbbeeeeegggggggeeeeeekkkkk', 'Beegeek'))
+
+Sample Output 1:
+
+True
+
+Sample Input 2:
+
+print(scrabble('begk', 'beegeek'))
+
+Sample Output 2:
+
+False
+
+Sample Input 3:
+
+print(scrabble('beegeek', 'beegeek'))
+
+Sample Output 3:
+
+True
+"""
+
+
 from collections import Counter
 
 def scrabble(symbols, word):
